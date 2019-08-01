f1f24fbbb09dcebddc4bdd9c8bd14fde9d00faf6
asafall <asafall21@gmail.com>
2019-08-01 17:01:10 +0300

    add equals implementation for index path

--- sectionedrecyclerview/src/main/java/com/asaf/sectionedrecyclerview/IndexPath.java
+++ sectionedrecyclerview/src/main/java/com/asaf/sectionedrecyclerview/IndexPath.java
@@ -1,6 +1,7 @@
 package com.asaf.sectionedrecyclerview;
 
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 
 public class IndexPath {
     static final int SECTION_ITEM_TYPE = 0;
@@ -30,5 +31,11 @@ public class IndexPath {
                 "section=" + section +
                 ", row=" + row +
                 "}";
+    }
+
+    @Override
+    public boolean equals(@Nullable Object obj) {
+        if (!(obj instanceof IndexPath)) return false;
+        return this.getSection() == ((IndexPath) obj).getSection() && this.getRow() == ((IndexPath) obj).getRow();
     }
 }
